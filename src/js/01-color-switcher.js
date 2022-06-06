1bc7eb8568766a0ed510aa75ab4205c4aff6def1
Alexander Pustosmekhov <a.pustosmekhov@gmail.com>
2022-06-06 19:15:22 +0300

    add task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+// Variables
+const buttonStartRef = document.querySelector('button[data-start]');
+const buttonStopRef = document.querySelector('button[data-stop]');
+const bodyRef = document.querySelector('body');
+let intervalId = null;
 
+// Event listeners
+buttonStartRef.addEventListener('click', startInterval);
+buttonStopRef.addEventListener('click', stopInterval);
+
+// Functions
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function changeColorBody() {
+  bodyRef.style.backgroundColor = getRandomHexColor();
+}
+
+function startInterval() {
+  changeAtributes(buttonStartRef, buttonStopRef);
+  intervalId = setInterval(changeColorBody, 1000);
+}
+
+function stopInterval() {
+  changeAtributes(buttonStopRef, buttonStartRef);
+  clearInterval(intervalId);
+}
+
+function changeAtributes(buttonSet, buttonRem) {
+  buttonSet.setAttribute('disabled', 'disabled');
+  buttonRem.removeAttribute('disabled', 'disabled');
+}
